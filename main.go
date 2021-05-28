c6cecee1835780e57bb72dd8f6e144980aa3ed4c
Joshua Zenn <wordman05@gmail.com>
2021-05-28 08:27:39 -0400

    Refactoring

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
@@ -13,72 +14,69 @@ import (
 )
 
 var (
-	cmd = "/opt/trafficserver/bin/traffic_ctl metric match host_status"
-	traffic_ctl = "/opt/trafficserver"
-	traffic_monitors = [...]string{"tm.example.com"}
-	api_path = "/api/cache-statuses"
+	cmd             = "/opt/trafficserver/bin/traffic_ctl metric match host_status"
+	trafficCtl      = "/opt/trafficserver"
+	trafficMonitors = [...]string{"tm.example.com"}
+	apiPath         = "/api/cache-statuses"
 
-	logLevel = zerolog.InfoLevel
-	logLocation ="/var/log/mid-health-check/mhc.log"
+	logLevel    = zerolog.InfoLevel
+	logLocation = "/var/log/mid-health-check/mhc.log"
+	logger      zerolog.Logger
 )
 
 func main() {
-	logger := getLogger()
-	hostStatus := getHostStatus(logger)
-	getTrafficMonitorStatus(hostStatus, logger)
+	initLogger()
+	trafficCtlStatus := pollTrafficCtlStatus()
+	hostStatus := getHostStatus(trafficCtlStatus)
+	rawTMResponse := getStatusFromTrafficMonitor()
+	tmStatus := parseTrafficMonitorStatus(rawTMResponse)
+	getTrafficMonitorStatus(hostStatus, tmStatus)
 }
 
-func getHostStatus(logger zerolog.Logger) (map[string]map[string]string) {
-	var host_status = map[string]map[string]string{}
+func getHostStatus(trafficCtlStatus string) map[string]map[string]string {
+	var hostStatus = map[string]map[string]string{}
+	for i, line := range strings.Split(trafficCtlStatus, "\n") {
+		logger.Trace().Str("line", line).Msgf("processing line %d from traffic_ctl output", i)
+		tmpLine := strings.Split(line, " ")
+		fqdn := strings.Replace(tmpLine[0], "proxy.process.host_status.", "", -1)
+		logger.Debug().Str("line", line).Str("fqdn", fqdn).Msg("got FQDN from traffic_ctl output")
+		hostname := strings.Split(fqdn, ".")[0]
+		logger.Debug().Str("line", line).Str("hostname", hostname).Msg("got FQDN from traffic_ctl output")
+		hostStatus[hostname] = buildHostStatusStruct(fqdn, tmpLine[1])
+	}
+	return hostStatus
+}
+
+func buildHostStatusStruct(fqdn string, statusLine string) map[string]string {
+	statusStruct := make(map[string]string)
+	statusStruct["fqdn"] = fqdn
+	for j, s := range strings.Split(statusLine, ",") {
+		logger.Trace().Str("s", s).Str("tmp_line", statusLine).Msgf("processing substring #%d from traffic_ctl output", j)
+		sSplit := strings.Split(s, ":")
+		if len(sSplit) > 1 {
+			logger.Trace().Str("s", s).Msg("split occurred")
+			statusStruct[sSplit[0]] = sSplit[1]
+		} else {
+			logger.Trace().Str("s", s).Msg("split did not occur")
+			statusStruct["STATUS"] = strings.Split(sSplit[0], "_")[2]
+		}
+	}
+	return statusStruct
+}
+
+func pollTrafficCtlStatus() string {
 	logger.Trace().Str("cmd", cmd).Msg("executing traffic_ctl")
 	out, err := exec.Command(cmd).Output()
 	if err != nil {
 		logger.Fatal().Err(err).Stack().Caller().Str("cmd", cmd).Msg("unable to execute traffic_ctl")
 	}
-	for i, line := range strings.Split(string(out), "\n") {
-		logger.Trace().Str("line", line).Msgf("processing line %d from traffic_ctl output", i)
-		tmp_line := strings.Split(line, " ")
-		fqdn := strings.Replace(tmp_line[0], "proxy.process.host_status.", "", -1)
-		logger.Debug().Str("line", line).Str("fqdn", fqdn).Msg("got FQDN from traffic_ctl output")
-		hostname := strings.Split(fqdn, ".")[0]
-		logger.Debug().Str("line", line).Str("hostname", hostname).Msg("got FQDN from traffic_ctl output")
-		host_status[hostname] = make(map[string]string)
-		host_status[hostname]["fqdn"] = fqdn
-		for j, s := range strings.Split(tmp_line[1], ",") {
-			logger.Trace().Str("s", s).Str("tmp_line", tmp_line[1]).Msgf("processing substring #%d from traffic_ctl output", j)
-			sSplit := strings.Split(s, ":")
-			if len(sSplit) > 1 {
-				logger.Trace().Str("s", s).Msg("split occurred")
-				host_status[hostname][sSplit[0]] = sSplit[1]
-			} else {
-				logger.Trace().Str("s", s).Msg("split did not occur")
-				host_status[hostname]["STATUS"] = strings.Split(sSplit[0], "_")[2]
-			}
-		}
-	}
-	return host_status
+	return string(out)
 }
 
-func getTrafficMonitorStatus(hostStatus map[string]map[string]string, logger zerolog.Logger) {
-	logger.Debug().Str("url", traffic_monitors[0] + api_path).Msg("connecting to TM")
-	r, err := http.Get(traffic_monitors[0] + api_path)
-	if err != nil {
-		logger.Fatal().Err(err).Stack().Caller().Str("url", traffic_monitors[0] + api_path).Msg("could not connect to TM")
-	}
-	defer r.Body.Close()
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		logger.Fatal().Err(err).Stack().Caller().Msg("unable to read response from TM")
-	}
-	var data = map[string]map[string]string{}
-	logger.Trace().Str("response", string(body)).Msg("unmarshalling response from TM")
-	err = json.Unmarshal(body, &data)
-	if err != nil {
-		logger.Fatal().Err(err).Stack().Caller().Str("response", string(body)).Msg("unable to parse response from TM")
-	}
-	for hostname, hostdata := range data {
+func getTrafficMonitorStatus(hostStatus map[string]map[string]string, tmStatus map[string]map[string]string) {
+	for hostname, hostdata := range tmStatus {
 		logger.Trace().Str("hostname", hostname).Str("hostdata", fmt.Sprint(hostdata)).Msg("processing host")
-		cmd = ""
+		updateCmd := ""
 		hType := hostdata["type"]
 		logger.Debug().Str("hostname", hostname).Str("type", hType).Msg("checking host type")
 		available := hostdata["combined_available"]
@@ -91,19 +89,19 @@ func getTrafficMonitorStatus(hostStatus map[string]map[string]string, logger zer
 				status := "UP"
 				if hostStatus[hostname]["MANUAL"] != status {
 					log.Info().Str("hostname", hostname).Msgf("%s: Traffic Monitor reports UP, Manual override is %s, Host Status is %s\n", hostname, hostStatus[hostname]["MANUAL"], hostStatus[hostname]["STATUS"])
-					cmd = fmt.Sprintf("%s host up %s", traffic_ctl, hostStatus[hostname]["fqdn"])
+					updateCmd = fmt.Sprintf("%s host up %s", trafficCtl, hostStatus[hostname]["fqdn"])
 				}
 			} else {
 				status := "DOWN"
 				logger.Trace().Str("hostname", hostname).Msg("host is not available")
 				if hostStatus[hostname]["MANUAL"] != status {
 					log.Info().Str("hostname", hostname).Msgf("%s: Traffic Monitor reports DOWN, Manual override is %s, Host Status is %s\n", hostname, hostStatus[hostname]["MANUAL"], hostStatus[hostname]["STATUS"])
-					cmd = fmt.Sprintf("%s host down %s", traffic_ctl, hostStatus[hostname]["fqdn"])
+					updateCmd = fmt.Sprintf("%s host down %s", trafficCtl, hostStatus[hostname]["fqdn"])
 				}
 			}
-			if cmd != "" {
-				logger.Debug().Str("hostname", hostname).Str("cmd", cmd).Msg("a command has been generated to modify host status")
-				out, err := exec.Command(cmd).Output()
+			if updateCmd != "" {
+				logger.Debug().Str("hostname", hostname).Str("cmd", updateCmd).Msg("a command has been generated to modify host status")
+				out, err := exec.Command(updateCmd).Output()
 				fmt.Printf("%s %s", out, err)
 			}
 		}
@@ -111,14 +109,44 @@ func getTrafficMonitorStatus(hostStatus map[string]map[string]string, logger zer
 	return
 }
 
-func getLogger() zerolog.Logger {
+func getStatusFromTrafficMonitor() string {
+	logger.Debug().Str("url", trafficMonitors[0]+apiPath).Msg("connecting to TM")
+	r, err := http.Get(trafficMonitors[0] + apiPath)
+	if err != nil {
+		logger.Fatal().Err(err).Stack().Caller().Str("url", trafficMonitors[0]+apiPath).Msg("could not connect to TM")
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			logger.Fatal().Err(err).Stack().Caller().Msg("unable to close connection with TM")
+		}
+	}(r.Body)
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		logger.Fatal().Err(err).Stack().Caller().Msg("unable to read response from TM")
+	}
+	return string(body)
+}
+
+func parseTrafficMonitorStatus(response string) map[string]map[string]string {
+	var data = map[string]map[string]string{}
+	logger.Trace().Str("response", response).Msg("unmarshalling response from TM")
+	err := json.Unmarshal([]byte(response), &data)
+	if err != nil {
+		logger.Fatal().Err(err).Stack().Caller().Str("response", response).Msg("unable to parse response from TM")
+	}
+	return data
+}
+
+func initLogger() {
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 	zerolog.SetGlobalLevel(logLevel)
 	logFile, err := os.OpenFile(logLocation, os.O_RDWR, 0644)
 	if err != nil {
 		log.Warn().Msgf("unable to open '%s':\n%w", logLocation, err)
-		return zerolog.New(os.Stdout).With().Timestamp().Logger()
+		logger = zerolog.New(os.Stdout).With().Timestamp().Logger()
+		return
 	}
 	multi := zerolog.MultiLevelWriter(logFile, os.Stdout)
-	return zerolog.New(multi).With().Timestamp().Logger()
+	logger = zerolog.New(multi).With().Timestamp().Logger()
 }
